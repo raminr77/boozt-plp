3650baaa593a424263c4895ce72ab4cc458f51bf
Ramin <r.rezaie@digikala.com>
2023-05-20 17:51:39 +0330

    fix: loading issues

--- src/app/layout/plp-layout/plp-layout-loading.tsx
+++ src/app/layout/plp-layout/plp-layout-loading.tsx
@@ -7,12 +7,12 @@ export function PlpLayoutLoading({ className }: GCommonCompnentProperties) {
     <div
       id='plp-loading'
       className={clsx(
-        'flex flex-col justify-center items-center w-full h-full top-0 left-0 bottom-0 right-0 absolute pointer-events-none z-10',
+        'flex flex-col items-center w-full h-full pt-10 top-0 left-0 bottom-0 right-0 absolute pointer-events-none z-30',
         styles.PlpLayout__loading,
         className
       )}
     >
-      <div className='loading mt-4'>
+      <div className='loading mt-10'>
         <div className='lds-ellipsis'>
           <div className='dark:bg-white' />
           <div className='dark:bg-white' />
